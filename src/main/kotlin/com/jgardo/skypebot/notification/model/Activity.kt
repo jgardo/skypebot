499a96a784422be09f93dbebc82a3a25bcb0e6b7
jakub.gardo <jakub.gardo@onwelo.com>
2018-04-18 21:00:34 +0200

    Ignore unknown properties

--- src/main/kotlin/com/jgardo/skypebot/notification/model/Activity.kt
+++ src/main/kotlin/com/jgardo/skypebot/notification/model/Activity.kt
@@ -1,8 +1,10 @@
 package com.jgardo.skypebot.notification.model
 
 import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class Activity @JsonCreator constructor(
         @JsonProperty("action") val action : String?,
         @JsonProperty("membersAdded") val membersAdded : List<Identificator>,
--- src/main/kotlin/com/jgardo/skypebot/notification/model/Identificator.kt
+++ src/main/kotlin/com/jgardo/skypebot/notification/model/Identificator.kt
@@ -1,8 +1,10 @@
 package com.jgardo.skypebot.notification.model
 
 import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class Identificator @JsonCreator constructor (
         @JsonProperty("id") val id : String,
         @JsonProperty("name") val name : String? = null
